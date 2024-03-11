04d67ce7a34bfcf11aac05d052b41d6963adc7c7
Joseceron101 <ESFE4612024@outlook.com>
2024-03-11 08:55:09 -0600

    Agregar archivos de proyecto.

---
+++ JACR20241103/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JACR20241103.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
